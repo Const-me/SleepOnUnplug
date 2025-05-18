9da0548ba36e7d1d51c68fe7373a2e52128d9e81
Const-me <const@const.me>
2025-05-18 17:34:21 +0200

    Default Win32 project

---
+++ SleepOnUnplug/framework.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
